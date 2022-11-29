66ab6c62716055791c1fe24a8b7cce02d7728fb9
Artur Castellanos <108274411+arturcastellanos@users.noreply.github.com>
2022-11-29 16:38:49 +0200

    delete line

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://arturcastellanos:covbes-gohrAx-dydxe8@cluster0.s7lsist.mongodb.net/todolistBD/?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://arturcastellanos:covbes-gohrAx-dydxe8@cluster0.s7lsist.mongodb.net/?retryWrites=true&w=majority")
 
 const item1 = new Item({
   name: "Welcome to your todolist!"
